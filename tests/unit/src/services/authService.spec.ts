ce36a4507fdd29229d9b85f52b004d44b5f5e55e
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-03-30 15:59:41 +0100

    test: :test_tube: Adding a new failing test to cover generateLogoutUrl functionality

--- tests/unit/src/services/authService.spec.ts
+++ tests/unit/src/services/authService.spec.ts
@@ -24,6 +24,8 @@ describe('AuthService', () => {
 
         it ('should store a state in session', () => {
             const session = '';
+            process.env.CLOUD_INSTANCE = 'myCloudInstance';
+            process.env.TENANT_ID = 'myTenantId';
 
             // let confidentialClientApplication: SinonStubbedInstance<ConfidentialClientApplication>;
             // confidentialClientApplication = sinon.createStubInstance(ConfidentialClientApplication);
@@ -33,12 +35,14 @@ describe('AuthService', () => {
 
             const authService = authServiceFactory.createAuthService(/* confidentialClientApplication, */ sessionData);
 
-            authService.getAuthCodeUrl(sessionData);
+            authService.generateAuthCodeUrl(sessionData);
             expect(sessionData.authState).not.to.be.eq('');
         })
 
         it ('should return a URL', () => {
             const session = '';
+            process.env.CLOUD_INSTANCE = 'myCloudInstance';
+            process.env.TENANT_ID = 'myTenantId';
 
             // let confidentialClientApplication: SinonStubbedInstance<ConfidentialClientApplication>;
             // confidentialClientApplication = sinon.createStubInstance(ConfidentialClientApplication);
@@ -47,7 +51,28 @@ describe('AuthService', () => {
 
             const authService = authServiceFactory.createAuthService(/* confidentialClientApplication */ sessionData);
 
-            expect(authService.getAuthCodeUrl(sessionData)).to.be.eq('https://www.example.com');
+            expect(authService.generateAuthCodeUrl(sessionData)).to.be.eq('myCloudInstance/myTenantId/oauth2/v2.0/authorize');
+        })
+    })
+
+    describe('Returns a Logout Url', () => {
+
+        const authServiceFactory = new AuthServiceFactory();
+
+        it ('should return a Logout Url', () => {
+            const session = '';
+            process.env.CLOUD_INSTANCE = 'myCloudInstance';
+            process.env.TENANT_ID = 'myTenantId';
+            process.env.POST_LOGOUT_REDIRECT_URI = 'myPostLogoutUri';
+
+            // let confidentialClientApplication: SinonStubbedInstance<ConfidentialClientApplication>;
+            // confidentialClientApplication = sinon.createStubInstance(ConfidentialClientApplication);
+
+            let sessionData = {} as SessionData;
+
+            const authService = authServiceFactory.createAuthService(/* confidentialClientApplication */ sessionData);
+
+            expect(authService.generateLogoutUrl()).to.be.eq('myCloudInstance/myTenantId/oauth2/v2.0/logout?post_logout_redirect_uri=myPostLogoutUri');
         })
     })
 })
