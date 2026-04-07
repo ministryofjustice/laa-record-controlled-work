97a5786ea2425b4647237b6b5f03c7cd7837c291
Drew Clayton <drew.clayton@justice.gov.uk>
2026-04-07 17:04:39 +0100

    Updated proxy ignores

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,2 +1,8 @@
 process.env.SESSION_SECRET ??= "test-secret";
 process.env.SESSION_NAME ??= "test-session";
+
+// Configure Axios to ignore proxies
+// SLSA uses safe-chain which proxies package managers
+// and sets HTTPS_PROXY on the environment, which breaks
+// tests.
+process.env.NO_PROXY ??= '*';
