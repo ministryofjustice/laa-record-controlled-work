bc223cfc95b8f4d5d06b988de7bce22e934a45ac
Drew Clayton <drew.clayton@justice.gov.uk>
2026-04-08 15:01:20 +0100

    Added NODE_ENV to test setup

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,5 +1,6 @@
 process.env.SESSION_SECRET ??= "test-secret";
 process.env.SESSION_NAME ??= "test-session";
+process.env.NODE_ENV ??= "test";
 
 // Configure Axios to ignore proxies
 // SLSA uses safe-chain which proxies package managers
