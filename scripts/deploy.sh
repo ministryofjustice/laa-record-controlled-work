2bd245674679ad754598a173d9a9ae2105fefe64
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-04-08 14:00:55 +0100

    Revert flag to --atomic, pipeline still using helm 3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,7 +31,7 @@ deploy_branch() {
 
 deploy_main() {  
   helm upgrade laa-record-controlled-work ./deploy/laa-record-controlled-work/. \
-                          --install --wait --rollback-on-failure \
+                          --install --wait --atomic \
                           --namespace="${K8S_NAMESPACE}" \
                           --values ./deploy/laa-record-controlled-work/values/"$ENVIRONMENT".yaml \
                           --set rcw.image.repository="$REGISTRY/$REPOSITORY" \
