cb59b991d8a6f7f749bb8b822ac07eba3354dd8a
Drew Clayton <drew.clayton@justice.gov.uk>
2026-04-15 17:28:01 +0100

    Added env variables to unit tests env

--- tests/unit/setup.ts
+++ tests/unit/setup.ts
@@ -1,7 +1,12 @@
 process.env.SESSION_SECRET ??= "test-secret";
 process.env.SESSION_NAME ??= "test-session";
 process.env.NODE_ENV ??= "test";
-
+process.env.CLIENT_ID ??= "test-client-id";
+process.env.TENANT_ID ??= "test-tenant-id";
+process.env.CLIENT_SECRET ??= "test-client-secret";
+process.env.REDIRECT_URI ??= "test-redirect-uri";
+process.env.CLOUD_INSTANCE ??= "test-cloud-instance";
+process.env.POST_LOGOUT_REDIRECT_URI ??= "test-post-logout-redirect-uri";
 // Configure Axios to ignore proxies
 // SLSA uses safe-chain which proxies package managers
 // and sets HTTPS_PROXY on the environment, which breaks
