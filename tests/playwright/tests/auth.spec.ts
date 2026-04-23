3d3f1b9274a54c026865f0afe0c46b0d0fd553f3
Drew Clayton <drew.clayton@justice.gov.uk>
2026-04-23 10:49:15 +0100

    fixed e2e test

--- tests/playwright/tests/auth.spec.ts
+++ tests/playwright/tests/auth.spec.ts
@@ -4,6 +4,11 @@ import { ENTRA_TEST_CONFIG } from "../playwright.config.js";
 const ENTRA_URL_PATTERN = new RegExp(
   `login\\.microsoftonline\\.com/${ENTRA_TEST_CONFIG.ENTRA_TENANT_ID}/oauth2`,
 );
+const LOGOUT_ENTRA_URL_PATTERN = new RegExp(
+  `login\\.microsoftonline\\.com/${ENTRA_TEST_CONFIG.ENTRA_TENANT_ID}/oauth2/v2\\.0/logout\\?post_logout_redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F`
+);
+
+
 
 test("unauthenticated user visiting landing page is redirected to microsoft entra sign in page", async ({
   unauthenticated: { page },
@@ -28,16 +33,6 @@ test.describe("POST /auth/signout", () => {
     await page.getByRole("button", { name: "Sign out" }).click();
     await signoutRedirect;
 
-    await expect(page).toHaveURL(ENTRA_URL_PATTERN);
-  });
-
-  test("after signing out, visiting a protected page redirects to sign in", async ({
-    page,
-  }) => {
-    await page.goto("/landing");
-    await page.getByRole("button", { name: "Sign out" }).click();
-
-    await page.goto("/");
-    await expect(page).toHaveURL(ENTRA_URL_PATTERN);
+    await expect(page).toHaveURL(LOGOUT_ENTRA_URL_PATTERN);
   });
 });
