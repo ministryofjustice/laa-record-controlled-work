9a88c631ee135c5bf4ff1fbc30c110a5f5996603
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-04-24 14:58:37 +0100

    Add an example global error handler for express

---
+++ middleware/globalErrorHandler.ts
@@ -0,0 +1,46 @@
+/* eslint-disable jsdoc/require-jsdoc -- not needed */
+import {
+  BAD_REQUEST,
+  INTERNAL_SERVER_ERROR,
+  NOT_FOUND,
+  SERVICE_UNAVAILABLE,
+  UNAUTHORIZED,
+} from "#src/lib/constants/httpStatus.js";
+import { DomainError } from "#src/lib/errors/domainError.js";
+import type { Request, Response } from "express";
+
+const ErrorStatuses: Record<string, number> = {
+  OAuthFailureError: UNAUTHORIZED,
+  DatabaseTimeoutError: SERVICE_UNAVAILABLE,
+  UserNotFoundError: NOT_FOUND,
+  ValidationFailureError: BAD_REQUEST,
+};
+
+// TODO: this is an example, it's not hooked up to the service yet.
+// needs to be properly scoped and tested before use.
+
+export function globalErrorHandler(
+  err: Error,
+  _: Request,
+  res: Response,
+): void {
+  if (process.env.NODE_ENV === "production") {
+    // this needs refining for specific error pages like unauthorised
+    res.redirect("/something-went-wrong");
+    return;
+  }
+
+  // need to consider handling for web requests and API requests separately
+  // for errors on CCQ data persistence endpoints
+
+  const statusCode = ErrorStatuses[err.name] || INTERNAL_SERVER_ERROR;
+  if (err instanceof DomainError) {
+    res.status(statusCode).json(err);
+    return;
+  }
+
+  res.status(statusCode).json({
+    name: err.name,
+    message: "Something went wrong.",
+  });
+}
