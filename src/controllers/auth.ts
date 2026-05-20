81c07dfa7335b9d5cc187ce8fa5248f07cda8cb5
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-05-20 23:08:01 +0100

    fix: correct order of destructured variables in processAuthCodeCallback

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -42,7 +42,7 @@ export const processAuthCodeCallback = async (
   next: NextFunction,
 ): Promise<void> => {
   try {
-    const { data, success } = authCodeResponseSchema.safeParse(req.query);
+    const { success, data } = authCodeResponseSchema.safeParse(req.query);
 
     if (!success) {
       res.status(BAD_REQUEST).send("Invalid redirect payload");
