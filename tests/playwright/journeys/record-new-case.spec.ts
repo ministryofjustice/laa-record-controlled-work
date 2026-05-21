082b3f033eaa40a0c694fc34dde6d187bf757b04
Rachel <rachel.dodson1@justice.gov.uk>
2026-05-21 11:55:41 +0100

    [574] Add full playwright test

--- tests/playwright/journeys/record-new-case.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "../fixtures/index.js";
-
-test("ecf step", async ({ page }) => {
-  // Navigate to the ECF page
-  await page.goto("/new-case/ecf");
-
-  // Check for the title of the application
-  await expect(page).toHaveTitle(/Does this case require Exceptional Case Funding\?/);
-});
