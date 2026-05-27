126695d3da9a5f8c4b75fa2910bd24dbde2b943a
Rachel <rachel.dodson1@justice.gov.uk>
2026-05-27 10:44:18 +0100

    [576] Add 6 month step

--- src/journeys/create-application/journey.ts
+++ src/journeys/create-application/journey.ts
@@ -5,6 +5,7 @@ import { JourneyEffects } from "#/journeys/effects.js";
 import { ineligibleStep } from "./steps/1-ecf-dropout.step.js";
 import { ecfStep } from "./steps/1-ecf.step.js";
 import { legalAidBeforeStep } from "./steps/2-legal-aid-before.step.js";
+import { legalAidBefore6MonthsStep } from "./steps/3-legal-aid-within-6-months.step.js";
 
 const journeyCode = "createApplication";
 
@@ -17,7 +18,7 @@ export const createApplicationJourney = journey({
   ],
   path: "/create-application",
   reachability: { disableReachabilityChecks: false },
-  steps: [ecfStep(journeyCode), ineligibleStep(journeyCode), legalAidBeforeStep(journeyCode)],
+  steps: [ecfStep(journeyCode), ineligibleStep(journeyCode), legalAidBeforeStep(journeyCode), legalAidBefore6MonthsStep(journeyCode)],
   title: "Record new case",
   view: { template: "partials/form-step" },
 });
---
+++ src/journeys/create-application/steps/3-legal-aid-within-6-months.step.ts
@@ -0,0 +1,89 @@
+import { JourneyEffects } from "#/journeys/effects.js";
+import {
+  step,
+  submit,
+  redirect,
+  Answer,
+  Condition,
+  validation,
+  Self,
+  Query,
+} from "@ministryofjustice/hmpps-forge/core/authoring";
+import { HtmlBlock } from "@ministryofjustice/hmpps-forge/core/components";
+import {
+  GovUKButton,
+  GovUKRadioInput,
+  GovUKBackLink,
+  GovUKCharacterCount,
+} from "@ministryofjustice/hmpps-forge/govuk-components";
+
+export const legalAidBefore6MonthsStep = (journeyCode: string): ReturnType<typeof step> => step({
+  path: "/legal-aid-last-6-months",
+  title: "Did your client get legal help for this matter in the last 6 months?",
+  blocks: [
+    GovUKBackLink({
+      href: "/new-case/legal-aid-before",
+    }),
+    HtmlBlock({
+      content: '<span class="govuk-caption-l">Client and case details</span>',
+    }),
+    GovUKRadioInput({
+      code: "legalAidLast6Months",
+      fieldset: {
+        legend: {
+          text: "Did your client get legal help for this matter in the last 6 months?",
+          isPageHeading: true,
+          classes: "govuk-fieldset__legend--l",
+        },
+      },
+      items: [
+        {
+          value: "yes",
+          text: "Yes",
+          block: GovUKCharacterCount({
+            code: "reasonForYes",
+            label:
+              "Explain the reason for creating a new case for the same matter",
+            maxLength: 500,
+            dependentWhen: Answer("legalAidBefore6Months").match(
+              Condition.Equals("yes"),
+            ),
+            validWhen: [
+              validation({
+                condition: Self().match(Condition.IsRequired()),
+                message:
+                  "Enter the reason you’re creating a new case for the same matter",
+              }),
+            ],
+          }),
+        },
+        {
+          value: "no",
+          text: "No",
+        },
+      ],
+      validWhen: [
+        validation({
+          condition: Self().match(Condition.IsRequired()),
+          message: "Select if your client got legal help for this matter in the last 6 months",
+        }),
+      ],
+    }),
+    GovUKButton({ text: "Continue" }),
+  ],
+  onSubmission: [
+    submit({
+      validate: true,
+      onValid: {
+        effects: [JourneyEffects.SaveDraftAnswers(journeyCode)],
+        next: [
+          redirect({
+            when: Query("returnTo").match(Condition.Equals("check-answers")),
+            goto: "check-answers",
+          }),
+          redirect({ goto: "client-details" }),
+        ],
+      },
+    }),
+  ],
+});
