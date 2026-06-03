a055711f625bc7042ff856ab288965d693a2e107
Drew Clayton <drew.clayton@justice.gov.uk>
2026-06-03 12:09:36 +0100

    renamed test files

--- src/journeys/create-application/journey.ts
+++ src/journeys/create-application/journey.ts
@@ -1,12 +1,12 @@
 import { access, journey } from "@ministryofjustice/hmpps-forge/core/authoring";
 
+import { clientDetailsStep } from "#/journeys/create-application/steps/4-client-details.step.js";
 import { JourneyEffects } from "#/journeys/effects.js";
 
 import { ineligibleStep } from "./steps/1-ecf-dropout.step.js";
 import { ecfStep } from "./steps/1-ecf.step.js";
 import { legalAidBeforeStep } from "./steps/2-legal-aid-before.step.js";
 import { legalAidBefore6MonthsStep } from "./steps/3-legal-aid-within-6-months.step.js";
-import { clientDetailsStep } from "#/journeys/create-application/steps/4-client-details.step.js";
 
 const journeyCode = "createApplication";
 
@@ -19,7 +19,13 @@ export const createApplicationJourney = journey({
   ],
   path: "/create-application",
   reachability: { disableReachabilityChecks: false },
-  steps: [ecfStep(journeyCode), ineligibleStep(journeyCode), legalAidBeforeStep(journeyCode), legalAidBefore6MonthsStep(journeyCode), clientDetailsStep(journeyCode)],
+  steps: [
+    ecfStep(journeyCode),
+    ineligibleStep(journeyCode),
+    legalAidBeforeStep(journeyCode),
+    legalAidBefore6MonthsStep(journeyCode),
+    clientDetailsStep(journeyCode),
+  ],
   title: "Record new case",
   view: { template: "partials/form-step" },
 });
--- src/journeys/create-application/steps/4-client-details.step.ts
+++ src/journeys/create-application/steps/4-client-details.step.ts
@@ -1,92 +1,96 @@
-import { JourneyEffects } from "#/journeys/effects.js";
 import {
+  Condition,
+  Query,
+  redirect,
+  Self,
   step,
   submit,
-  redirect,
-  Condition,
   validation,
-  Self,
-  Query,
 } from "@ministryofjustice/hmpps-forge/core/authoring";
 import { HtmlBlock } from "@ministryofjustice/hmpps-forge/core/components";
 import {
-  GovUKButton,
-  GovUKHeading,
   GovUKBackLink,
-  GovUKTextInput,
+  GovUKButton,
   GovUKDateInputFull,
+  GovUKHeading,
+  GovUKTextInput,
   GovUKUtilityClasses,
   GovUKValidations,
 } from "@ministryofjustice/hmpps-forge/govuk-components";
 
-export const clientDetailsStep = (journeyCode: string): ReturnType<typeof step> => step({
-  path: "/client-details",
-  title: "Your client's details",
-  blocks: [
-    GovUKBackLink({
-      href: "/new-case/legal-aid-before",
-    }),
-    HtmlBlock({
-      content: '<span class="govuk-caption-l">Client and case details</span>',
-    }),
-    GovUKHeading({
-      text: "Your client's details",
-    }),
-    GovUKTextInput({
-      code: "fullName",
-      label: {
-        text: "Full name",
-        isPageHeading: false,
-        classes: "govuk-label--m",
-      },
-      validWhen: [
-        validation({
-          condition: Self().match(Condition.IsRequired()),
-          message: "Enter your client's name",
-        }),
-      ],
-    }),
-    GovUKDateInputFull({
-      code: "dateOfBirth",
-      fieldset: {
-        legend: {
-          text: "Date of birth",
+import { JourneyEffects } from "#/journeys/effects.js";
+
+export const clientDetailsStep = (
+  journeyCode: string,
+): ReturnType<typeof step> =>
+  step({
+    blocks: [
+      GovUKBackLink({
+        href: "/new-case/legal-aid-before",
+      }),
+      HtmlBlock({
+        content: '<span class="govuk-caption-l">Client and case details</span>',
+      }),
+      GovUKHeading({
+        text: "Your client's details",
+      }),
+      GovUKTextInput({
+        code: "fullName",
+        label: {
+          classes: "govuk-label--m",
           isPageHeading: false,
-          classes: GovUKUtilityClasses.Fieldset.MediumLabel,
+          text: "Full name",
         },
-      },
-      hint: {
-        text: "For example, 31 3 1980",
-      },
-      validWhen: [
-        ...GovUKValidations.DateInputFull({
-          empty: { message: "Enter your client's date of birth" },
-          missingDay: { message: "Date of birth must include a day" },
-          missingMonth: { message: "Date of birth must include a month" },
-          missingYear: { message: "Date of birth must include a year" },
-          invalid: { message: "Date of birth must be a real date" },
-          mustBePast: {
-            message: "Date of birth must be in the past",
-            submissionOnly: true,
-          },
-        }),
-      ],
-    }),
-    GovUKButton({ text: "Continue" }),
-  ],
-  onSubmission: [
-    submit({
-      validate: true,
-      onValid: {
-        effects: [JourneyEffects.SaveDraftAnswers(journeyCode)],
-        next: [
-          redirect({
-            when: Query("returnTo").match(Condition.Equals("check-answers")),
-            goto: "check-answers",
+        validWhen: [
+          validation({
+            condition: Self().match(Condition.IsRequired()),
+            message: "Enter your client's name",
           }),
-          redirect({ goto: "ni-number" }),
         ],
-      },
-    }),
-  ],
-});
+      }),
+      GovUKDateInputFull({
+        code: "dateOfBirth",
+        fieldset: {
+          legend: {
+            classes: GovUKUtilityClasses.Fieldset.MediumLabel,
+            isPageHeading: false,
+            text: "Date of birth",
+          },
+        },
+        hint: {
+          text: "For example, 31 3 1980",
+        },
+        validWhen: [
+          ...GovUKValidations.DateInputFull({
+            empty: { message: "Enter your client's date of birth" },
+            invalid: { message: "Date of birth must be a real date" },
+            missingDay: { message: "Date of birth must include a day" },
+            missingMonth: { message: "Date of birth must include a month" },
+            missingYear: { message: "Date of birth must include a year" },
+            mustBePast: {
+              message: "Date of birth must be in the past",
+              submissionOnly: true,
+            },
+          }),
+        ],
+      }),
+      GovUKButton({ text: "Continue" }),
+    ],
+    onSubmission: [
+      submit({
+        onValid: {
+          effects: [JourneyEffects.SaveDraftAnswers(journeyCode)],
+          next: [
+            redirect({
+              goto: "check-answers",
+              when: Query("returnTo").match(Condition.Equals("check-answers")),
+            }),
+            redirect({ goto: "ni-number" }),
+          ],
+        },
+        validate: true,
+      }),
+    ],
+    path: "/client-details",
+    title: "Your client's details",
+  });
