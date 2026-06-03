e21e4f795f3267de6ec6f9578ffa3185fb7b157b
Drew Clayton <drew.clayton@justice.gov.uk>
2026-06-03 16:44:14 +0100

    updated integration test

--- tests/integration/journeys/create-application/6-have-a-home-address.step.spec.ts
+++ tests/integration/journeys/create-application/6-have-a-home-address.step.spec.ts
@@ -33,18 +33,41 @@ function createClient() {
 }
 
 describe("Have A Home Address Step", () => {
-
   const client = createClient();
 
-  it("should render the have a home address form on GET", async () => {
-    const result = await client.get("/create-application/have-a-home-address");
+  it.only("should render the have a home address form on GET", async () => {
+    const result: TestResult = await client.get(
+      "/create-application/have-a-home-address",
+    );
     expect(result.type).to.equal("render");
+    if (result.type === "render") {
+      
+      // Check that the page title is correct
+      expect(result.context.step.title).to.equal(
+        "Does your client have a home address?",
+      );
+      // Check that the radio options are correct
+      const radios = result.getBlocksByVariant("govukRadioInput");
+      const buttons = radios[0].properties.items as { text: string;}[];
+      expect(buttons.length).to.equal(2);
+      expect(buttons[0].text).to.equal("Yes");
+      expect(buttons[1].text).to.equal("No, they have no fixed address");
+
+      // Check that the hint text is correct
+      const hint = radios[0].properties.hint as { text: string };
+      expect(hint.text).to.equal(
+        "The home address is the place that they normally live in, and sometimes called the main dwelling.",
+      );
+    }
   });
 
   it("should show validation error if no option is selected", async () => {
-    const result = await client.post("/create-application/have-a-home-address", {
-      body: {},
-    });
+    const result = await client.post(
+      "/create-application/have-a-home-address",
+      {
+        body: {},
+      },
+    );
     expect(result.type).to.equal("render");
     if (result.type === "render") {
       expect(result.context.showValidationFailures).to.equal(true);
@@ -55,11 +78,14 @@ describe("Have A Home Address Step", () => {
   });
 
   it("should redirect to Enter address manually step if yes", async () => {
-    const result: TestResult = await client.post("/create-application/have-a-home-address", {
-      body: {
-        haveAHomeAddress: "yes",
+    const result: TestResult = await client.post(
+      "/create-application/have-a-home-address",
+      {
+        body: {
+          haveAHomeAddress: "yes",
+        },
       },
-    });
+    );
     expect(result.type).to.equal("redirect");
     if (result.type === "redirect") {
       expect(result.url).to.equal("/create-application/enter-address-manually");
@@ -67,11 +93,14 @@ describe("Have A Home Address Step", () => {
   });
 
   it("should redirect to Need means assessment step if no", async () => {
-    const result = await client.post("/create-application/have-a-home-address", {
-      body: {
-        haveAHomeAddress: "no",
+    const result = await client.post(
+      "/create-application/have-a-home-address",
+      {
+        body: {
+          haveAHomeAddress: "no",
+        },
       },
-    });
+    );
     expect(result.type).to.equal("redirect");
     if (result.type === "redirect") {
       expect(result.url).to.equal("/create-application/need-means-assessment");
