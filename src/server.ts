c438ab55345f3a98a7e5536878c9f9876e7a99e9
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-06-03 22:52:25 +0100

    Remove ascii banner

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import createApp from "#/app.js";
 import config from "#/config.js";
-import { displayAsciiBanner } from "#/lib/displayAsciiBanner.js";
 import { logger } from "#/logger.js";
 
 const app = await createApp();
 
-displayAsciiBanner(config);
-
 app.listen(config.app.port, () => {
+  logger.info(`Service: ${config.app.service.name}`);
+  logger.info(`Server is running at: http://localhost:${config.app.port}`);
   logger.info(`Listening on port ${config.app.port}...`);
 });
