a726793a17014ea8252134274a9f4d229a31b043
Rachel <rachel.dodson1@justice.gov.uk>
2026-06-08 09:46:56 +0100

    [849] add unit tests for save and load

---
+++ tests/unit/src/journeys/effects.spec.ts
@@ -0,0 +1,111 @@
+import { expect } from "chai";
+import { describe, it } from "mocha";
+import { createForgeTestClient } from "../../../integration/utils/helpers.js";
+import { ecfStep } from "#/journeys/create-application/steps/1-ecf.step.js";
+import { TestRenderResult } from "@ministryofjustice/hmpps-forge/core/testing";
+import { JourneyEffects } from "#/journeys/effects.js";
+
+const client = createForgeTestClient(ecfStep("testJourney"));
+const session: Record<string, unknown> = {};
+
+describe("SaveDraftAnswers", () => {
+  it("persists draft answers in the session under the journey key", async () => {
+    await client.post("/create-application/ecf", {
+      session,
+      body: { ecf: "yes" },
+    });
+    const drafts = session.journeyDrafts as Record<
+      string,
+      Record<string, unknown>
+    >;
+
+    expect(drafts?.testJourney?.ecf).to.equal("yes");
+  });
+
+  it("preserves draft answers from other journeys", async () => {
+    session.journeyDrafts = {
+      anotherJourney: { someAnswer: "yes" },
+    };
+
+    await client.post("/create-application/ecf", {
+      session,
+      body: { ecf: "yes" },
+    });
+
+    const drafts = session.journeyDrafts as Record<
+      string,
+      Record<string, unknown>
+    >;
+
+    expect(drafts?.testJourney?.ecf).to.equal("yes");
+    expect(drafts?.anotherJourney?.someAnswer).to.equal("yes");
+  });
+
+  it("merges with existing draft answers for the same journey", async () => {
+    session.journeyDrafts = {
+      testJourney: { existingAnswer: "foo" },
+    };
+
+    await client.post("/create-application/ecf", {
+      session,
+      body: { ecf: "yes" },
+    });
+
+    const drafts = session.journeyDrafts as Record<
+      string,
+      Record<string, unknown>
+    >;
+
+    expect(drafts?.testJourney?.existingAnswer).to.equal("foo");
+    expect(drafts?.testJourney?.ecf).to.equal("yes");
+  });
+
+  it('overwrites existing draft answer for the same question', async () => {
+    session.journeyDrafts = {
+      testJourney: { ecf: "no" },
+    };
+
+    await client.post("/create-application/ecf", {
+      session,
+      body: { ecf: "yes" },
+    });
+
+    const drafts = session.journeyDrafts as Record<
+      string,
+      Record<string, unknown>
+    >;
+
+    expect(drafts?.testJourney?.ecf).to.equal("yes");
+  });
+});
+
+describe("LoadDraftAnswers", () => {
+  it("loads draft answers from the session into the context", async () => {
+    session.journeyDrafts = {
+      testJourney: { ecf: "yes" },
+    };
+
+    const result = await client.get("/create-application/ecf", {
+      session,
+    });
+    expect(result.type).to.equal("render");
+    const renderResult = result as TestRenderResult;
+    const [radioInput] = renderResult.getBlocksByVariant("govukRadioInput");
+
+    expect(radioInput.properties.value).to.equal("yes");
+  });
+
+  it("does does nothing when no draft exists for this journey", async () => {
+    session.journeyDrafts = {
+      testJourney: {},
+    };
+    const result = await client.get("/create-application/ecf", {
+      session,
+    });
+    expect(result.type).to.equal("render");
+    const renderResult = result as TestRenderResult;
+    const [radioInput] = renderResult.getBlocksByVariant("govukRadioInput");
+
+    expect(radioInput.properties.value).to.equal(undefined);
+  });
+});
