bcfcad0a12fa54e038c26e970a1c17eea422124d
Drew Clayton <drew.clayton@justice.gov.uk>
2026-06-25 11:31:48 +0100

    updated type

--- src/journeys/journey.types.ts
+++ src/journeys/journey.types.ts
@@ -1,10 +1,22 @@
-import type { GovUKTaskList } from "@ministryofjustice/hmpps-forge/govuk-components";
-
 export enum Status {
   CannotStart,
   Completed,
   Incomplete,
 }
 
-// eslint-disable-next-line @typescript-eslint/no-magic-numbers -- created type to match forge 0 is the index we need
-export type TaskListItem = Parameters<typeof GovUKTaskList>[0]["items"][number];
+// 25/06/25 forge currently doesnt export type TaskListItem so we have loosely defined it here.
+// Once forge exports the type we can remove this and import it from forge instead.
+export interface TaskListItem {
+  href?: string;
+  status: {
+    classes?: string;
+    tag?: {
+      classes: string;
+      text: string;
+    };
+    text?: string;
+  };
+  title: {
+    text: string;
+  };
+}
