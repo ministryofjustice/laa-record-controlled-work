8a3204557f54b6dc93b831ae272ee609a023aee3
Drew Clayton <drew.clayton@justice.gov.uk>
2026-07-03 10:34:42 +0100

    updated your cases effects

---
+++ src/journeys/your-cases/your-cases.effects.ts
@@ -0,0 +1,42 @@
+import {
+  defineEffectFunctions,
+  type EffectFunctionExpr,
+} from "@ministryofjustice/hmpps-forge/core/authoring";
+
+import type { getApplications } from "#/api/client/schema/applications/applications.gen.js";
+
+import { Applications } from "#/api/client/model/applications.zod.gen.js";
+import { HTTP_STATUS } from "#/lib/constants/http.js";
+
+export interface YourCasesEffectsDeps {
+  getApplications: typeof getApplications;
+}
+
+interface YourCasesEffectShape {
+  LoadYourCaseList: () => EffectFunctionExpr;
+}
+
+export const {
+  effects: YourCasesEffects,
+  implementations: YourCasesEffectImplementations,
+} = defineEffectFunctions<YourCasesEffectShape, YourCasesEffectsDeps>({
+  LoadYourCaseList:
+    ({ getApplications }) =>
+    async (context) => {
+      const response = await getApplications();
+
+      if (response.status !== HTTP_STATUS.OK) {
+        // TODO how to handle
+        return;
+      }
+
+      const result = Applications.safeParse(response.data);
+      if (!result.success) {
+        // TODO how to handle
+        return;
+      }
+      const caseList: Applications = result.data;
+
+      context.setData("caseList", caseList);
+    },
+});
