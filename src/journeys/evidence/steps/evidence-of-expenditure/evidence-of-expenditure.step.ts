730a306a60a475c5794a40cbdf4b9336b42e1a1b
Rachel <rachel.dodson1@justice.gov.uk>
2026-07-14 17:14:43 +0100

    [966] Add evidence step

---
+++ src/journeys/evidence/steps/evidence-of-expenditure/evidence-of-expenditure.step.ts
@@ -0,0 +1,60 @@
+import {
+  Condition,
+  Query,
+  redirect,
+  step,
+  submit,
+} from "@ministryofjustice/hmpps-forge/core/authoring";
+
+import { JourneyEffects } from "#/journeys/effects.js";
+import {
+  backLink,
+  caption,
+  continueButton,
+} from "#/journeys/evidence/common.blocks.js";
+import {
+  description,
+  heading,
+  housingCostsEvidenceGroup,
+  childCareEvidenceGroup,
+  employedEvidenceGroup,
+  maintenanceEvidenceGroup,
+  label,
+} from "#/journeys/evidence/steps/evidence-of-expenditure/evidence-of-expenditure.blocks.js";
+import { t } from "#/lib/i18n.js";
+
+export const evidenceOfExpenditure = (
+  journeyCode: string,
+): ReturnType<typeof step> =>
+  step({
+    blocks: [
+      backLink("/cases/evidence/evidence-of-income"),
+      caption,
+      heading,
+      description,
+      label,
+      employedEvidenceGroup,
+      housingCostsEvidenceGroup,
+      childCareEvidenceGroup,
+      maintenanceEvidenceGroup,
+      continueButton,
+    ],
+    onSubmission: [
+      submit({
+        onValid: {
+          effects: [JourneyEffects.SaveDraftAnswers(journeyCode)],
+          next: [
+            redirect({
+              goto: "check-answers",
+              when: Query("returnTo").match(Condition.Equals("check-answers")),
+            }),
+            redirect({ goto: "evidence-of-capital" }),
+          ],
+        },
+        validate: true,
+      }),
+    ],
+    path: "/evidence-of-expenditure",
+    reachability: { entryWhen: true },
+    title: t("journeys.evidence.evidenceOfExpenditure.title"),
+  });
