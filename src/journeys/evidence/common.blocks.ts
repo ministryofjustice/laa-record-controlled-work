92c67efa778ff63f0e6f90d2d98e4b8755ce695f
Rachel <rachel.dodson1@justice.gov.uk>
2026-07-17 11:47:26 +0100

    [897] update import

--- src/journeys/evidence/common.blocks.ts
+++ src/journeys/evidence/common.blocks.ts
@@ -3,7 +3,7 @@ import {
   GovUKBackLink,
   GovUKButton,
 } from "@ministryofjustice/hmpps-forge/govuk-components";
-import { t } from "i18next";
+import { t } from "#/lib/i18n.js";
 
 export const backLink = (url: string): GovUKBackLink =>
   GovUKBackLink({
