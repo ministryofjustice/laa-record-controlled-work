1bd00456eced49efb8d4739bbff5bab5ccbebdc9
Drew Clayton <drew.clayton@justice.gov.uk>
2026-07-20 18:15:01 +0100

    fix linting

--- src/journeys/your-cases/steps/your-cases/your-cases.step.ts
+++ src/journeys/your-cases/steps/your-cases/your-cases.step.ts
@@ -8,12 +8,12 @@ import {
   createCaseButton,
   heading,
 } from "#/journeys/your-cases/common.blocks.js";
+import { YourCasesEffects } from "#/journeys/your-cases/effects/registry.js";
 import {
   casesTable,
   noCasesMessage,
   subNavigation,
 } from "#/journeys/your-cases/steps/your-cases/your-cases.blocks.js";
-import { YourCasesEffects } from "#/journeys/your-cases/effects/registry.js";
 import { t } from "#/lib/i18n.js";
 
 export const yourCasesStep = step({
