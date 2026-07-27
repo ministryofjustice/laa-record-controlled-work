6d3161395e45b4759288438204843a00846b8111
Kyle Jones <kyle.jones2@justice.gov.uk>
2026-07-27 09:48:59 +0100

    chore: :rotating_light: Fixing linter errors for import ordering in create-application.journey.ts

--- src/journeys/create-application/create-application.journey.ts
+++ src/journeys/create-application/create-application.journey.ts
@@ -1,18 +1,18 @@
 import { access, journey } from "@ministryofjustice/hmpps-forge/core/authoring";
 
-import { declarationStep } from "#/journeys/create-application/steps/declaration.step.js";
-import { legalAidLast6MonthsStep } from "#/journeys/create-application/steps/legal-aid-last-6-months.step.js";
 import { clientDetailsStep } from "#/journeys/create-application/steps/client-details.step.js";
-import { haveAHomeAddressStep } from "#/journeys/create-application/steps/have-a-home-address.step.js";
+import { declarationStep } from "#/journeys/create-application/steps/declaration.step.js";
 import { enterAddressManuallyStep } from "#/journeys/create-application/steps/enter-address-manually.step.js";
 import { enterOverseasAddressStep } from "#/journeys/create-application/steps/enter-overseas-address.step.js";
+import { haveAHomeAddressStep } from "#/journeys/create-application/steps/have-a-home-address.step.js";
+import { legalAidLast6MonthsStep } from "#/journeys/create-application/steps/legal-aid-last-6-months.step.js";
 import { JourneyEffects } from "#/journeys/effects.js";
 
+import { checkAnswersStep } from "./steps/check-answers.step.js";
 import { ineligibleStep } from "./steps/ecf-dropout.step.js";
 import { ecfStep } from "./steps/ecf.step.js";
 import { legalAidBeforeStep } from "./steps/legal-aid-before.step.js";
 import { niNumberStep } from "./steps/ni-number.step.js";
-import { checkAnswersStep } from "./steps/check-answers.step.js";
 
 const journeyCode = "createApplication";
 
