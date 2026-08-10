f372abf84d1f11c6616426d8e8ed3faf8ade6d17
Drew Clayton <drew.clayton@justice.gov.uk>
2026-08-10 17:36:54 +0100

    update jsdoc on countries

--- src/lib/countries.ts
+++ src/lib/countries.ts
@@ -10,8 +10,18 @@ const COUNTRY_NAME_BY_CODE = new Map(
   countries.map((country) => [country.code, country.name]),
 );
 
+/**
+ * Convert country name to ISO 3166 alpha-2 country code.
+ * @param country Country name.
+ * @returns string ISO 3166 alpha-2 country code.
+ */
 export const mapCountryNameToIsoCode = (country: string): string =>
   COUNTRY_CODE_BY_NAME.get(country) ?? country;
 
+/**
+ * Convert ISO 3166 alpha-2 country code to country name.
+ * @param countryCode ISO 3166 alpha-2 country code.
+ * @returns string Country name.
+ */
 export const mapIsoCodeToCountryName = (countryCode: string): string =>
   COUNTRY_NAME_BY_CODE.get(countryCode) ?? countryCode;
