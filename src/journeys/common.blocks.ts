9a7c1c3bceb9fe71093a9bc2296e2509f534f05f
Rachel <rachel.dodson1@justice.gov.uk>
2026-08-19 12:07:08 +0100

    [1265] Add view-application journey

--- src/journeys/common.blocks.ts
+++ src/journeys/common.blocks.ts
@@ -1,5 +1,11 @@
-import { GovUKButton } from "@ministryofjustice/hmpps-forge/govuk-components";
+import { GovUKBackLink, GovUKButton } from "@ministryofjustice/hmpps-forge/govuk-components";
 
 import { t } from "#/lib/i18n.js";
+import { ResolvableString } from "@ministryofjustice/hmpps-forge/core/components";
 
 export const continueButton = GovUKButton({ text: t("common.continue") });
+
+export const backLink = (url: ResolvableString): GovUKBackLink =>
+  GovUKBackLink({
+    href: url,
+  });
