291da964836c7eaa3ecf7523a3854c4d5e57361a
Drew Clayton <drew.clayton@justice.gov.uk>
2026-08-21 09:15:21 +0100

    remove formatters

--- src/journeys/create-application/steps/declaration/declaration.formatters.ts
+++
@@ -1,6 +0,0 @@
-import { t } from "#/lib/i18n.js";
-
-export const declaration = t("journeys.createApplication.declaration.title");
-export const agreeAndContinue = t(
-  "journeys.createApplication.declaration.continue",
-);
