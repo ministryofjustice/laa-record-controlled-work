7ee9ac6b3850825b6ebbb9ed3f29a98912f9cf7c
Drew Clayton <drew.clayton@justice.gov.uk>
2026-08-21 09:15:21 +0100

    remove formatters

--- src/journeys/create-application/steps/declaration/declaration.step.ts
+++ src/journeys/create-application/steps/declaration/declaration.step.ts
@@ -5,25 +5,27 @@ import {
 } from "@ministryofjustice/hmpps-forge/core/authoring";
 
 import { body } from "#/journeys/create-application/steps/declaration/declaration.blocks.js";
-import {
-  agreeAndContinue,
-  declaration,
-} from "#/journeys/create-application/steps/declaration/declaration.formatters.js";
 import { backLink, button, heading } from "#/journeys/shared.blocks.js";
 import { StepCode } from "#/journeys/StepCode.enum.js";
+import { t } from "#/lib/i18n.js";
+
+const TITLE = t("journeys.createApplication.declaration.title");
+
+const AGREE_AND_CONTINUE = t("journeys.createApplication.declaration.continue");
+const ROOT = "/";
 
 export const declarationStep = (): ReturnType<typeof step> =>
   step({
     blocks: [
-      backLink("/"),
-      heading(declaration),
+      backLink(ROOT),
+      heading(TITLE),
       body(),
-      button(agreeAndContinue),
+      button(AGREE_AND_CONTINUE),
     ],
     onSubmission: [onSubmission],
     path: "/provider-declaration",
     reachability: { entryWhen: true },
-    title: declaration,
+    title: TITLE,
   });
 
 const onSubmission = submit({
