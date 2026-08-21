599b7bc6cf7363dd70178a38d39fc8ed169ad8d4
Drew Clayton <drew.clayton@justice.gov.uk>
2026-08-21 09:32:59 +0100

    remove reformatter

--- src/journeys/create-application/steps/ecfDropout/ecfDropout.step.ts
+++ src/journeys/create-application/steps/ecfDropout/ecfDropout.step.ts
@@ -4,32 +4,33 @@ import {
   submit,
   type SubmitHook,
 } from "@ministryofjustice/hmpps-forge/core/authoring";
+import { t } from "i18next";
 
 import { CreateApplicationEffects } from "#/journeys/create-application/create-application.effects.js";
 import {
   ecfDroupoutBody,
   submitFormsBody,
 } from "#/journeys/create-application/steps/ecfDropout/ecfDropout.blocks.js";
-import {
-  ecfDropoutHeading,
-  ecfDropoutTitle,
-  ecfPath,
-  returnToCaseList,
-} from "#/journeys/create-application/steps/ecfDropout/ecfDropout.formatters.js";
+import { JourneyPath } from "#/journeys/JourneyPath.enum.js";
 import { backLink, button, heading } from "#/journeys/shared.blocks.js";
+
+const ECF_PATH = `${JourneyPath.CREATE_APPLICATION}/ecf`;
+const HEADING = t("journeys.createApplication.ecfDropout.heading");
+const TITLE = t("journeys.createApplication.ecfDropout.title");
+const RETURN_TO_CASE_LIST = t("journeys.createApplication.ecfDropout.button");
 
 export const ineligibleStep = (journeyCode: string): ReturnType<typeof step> =>
   step({
     blocks: [
-      backLink(ecfPath),
-      heading(ecfDropoutHeading),
+      backLink(ECF_PATH),
+      heading(HEADING),
       ecfDroupoutBody(),
       submitFormsBody(),
-      button(returnToCaseList),
+      button(RETURN_TO_CASE_LIST),
     ],
     onSubmission: [onSubmission(journeyCode)],
     path: "/ecf-dropout",
-    title: ecfDropoutTitle,
+    title: TITLE,
   });
 
 const onSubmission = (journeyCode: string): SubmitHook =>
