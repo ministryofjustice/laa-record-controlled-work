2f8d7450004bd2e7d4af57be7c1f812b54b07b3b
Drew Clayton <drew.clayton@justice.gov.uk>
2026-08-26 16:10:35 +0100

    updated en.json

--- src/journeys/create-application/steps/declaration/declaration.blocks.ts
+++ src/journeys/create-application/steps/declaration/declaration.blocks.ts
@@ -2,6 +2,10 @@ import type { HtmlBlock } from "@ministryofjustice/hmpps-forge/core/components";
 
 import { GovUKBody } from "@ministryofjustice/hmpps-forge/govuk-components";
 
+import { t } from "#/lib/i18n.js";
+
+const DECLARATION_BODY = t("journeys.createApplication.declaration.text");
+
 /**
  * Renders the declaration consent body block for the create application journey.
  *
@@ -10,7 +14,6 @@ import { GovUKBody } from "@ministryofjustice/hmpps-forge/govuk-components";
 export function body(): HtmlBlock {
   return GovUKBody({
     classes: "govuk-body",
-    // TODO - this should be in the i18n file, but it contains HTML which is not supported by the i18n library
-    text: 'By continuing, you agree that:<br><ul class="govuk-list govuk-list--bullet govuk-!-margin-bottom-6"><li>your client has instructed you, the provider, to act on their behalf</li><li>your client has read the <a target="_blank" href="/privacy-policy">LAA privacy policy (opens in a new window or tab)</a></li><li>you\'ll give complete and correct information</li></ul>',
+    text: DECLARATION_BODY,
   });
 }
