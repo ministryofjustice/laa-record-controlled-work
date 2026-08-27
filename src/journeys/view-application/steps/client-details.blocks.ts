100ce5f183263f45001417b10cc59a7da0d329c4
Rachel <rachel.dodson1@justice.gov.uk>
2026-08-27 15:17:54 +0100

    [1266] Add subnav

--- src/journeys/view-application/steps/client-details.blocks.ts
+++ src/journeys/view-application/steps/client-details.blocks.ts
@@ -1,75 +1,23 @@
-import { Format } from "@ministryofjustice/hmpps-forge/core/authoring";
-import {
-  HtmlBlock,
-  type ResolvableString,
-} from "@ministryofjustice/hmpps-forge/core/components";
-import {
-  GovUKBody,
-  GovUKButton,
-  GovUKHeading,
-} from "@ministryofjustice/hmpps-forge/govuk-components";
+import { t } from "#/lib/i18n.js";
+import { MOJSubNavigation } from "@ministryofjustice/hmpps-forge/moj-components";
 
-/**
- * Builds a GovUKBody block for the case reference number.
- * @param caseRefNumber - The text content for the Body.
- * @returns A GovUKBody block definition.
- */
-export function caseReferenceNumber(
-  caseRefNumber: ResolvableString,
-): ReturnType<typeof GovUKBody> {
-  return GovUKBody({
-    classes: "govuk-!-margin-bottom-1",
-    size: "l",
-    text: Format("<strong>Reference number:</strong> %1", caseRefNumber),
+export function subNavigation(): ReturnType<typeof MOJSubNavigation> {
+  return MOJSubNavigation({
+    items: [
+      {
+        active: true,
+        href: "client-details",
+        text: t("pages.view.tabs.ClientDetails"),
+      },
+      {
+        href: "means-assessment",
+        text: t("pages.view.tabs.meansAssessment"),
+      },
+      {
+        href: "evidence",
+        text: t("pages.view.tabs.evidence"),
+      },
+    ],
   });
 }
 
-/**
- * Builds a heading block.
- * @param text - The text content for the heading.
- * @returns A heading block definition.
- */
-export function heading(
-  text: ResolvableString,
-): ReturnType<typeof GovUKHeading> {
-  return GovUKHeading({
-    size: "xl",
-    text,
-  });
-}
-
-/**
- * Builds a GovUKButton for printing the case, non functional.
- * @returns A GovUKButton block definition for printing the case.
- */
-export function printButton(): ReturnType<typeof GovUKButton> {
-  return GovUKButton({
-    classes: "govuk-button--secondary",
-    text: "Print this case",
-  });
-}
-
-/**
- * Builds a GovUKBody block for the recorded on date.
- * @param recordedOnDate - The text content for the recorded on date.
- * @returns A GovUKBody block definition for the recorded on date.
- */
-export function recordedOn(
-  recordedOnDate: ResolvableString,
-): ReturnType<typeof GovUKBody> {
-  return GovUKBody({
-    size: "l",
-    text: Format("<strong>Recorded on:</strong> %1", recordedOnDate),
-  });
-}
-
-/**
- * Builds a status tag block.
- * @param text - The text content for the status tag.
- * @returns A status tag block definition.
- */
-export function statusTag(text: string): ReturnType<typeof HtmlBlock> {
-  return HtmlBlock({
-    content: `<div class="govuk-!-margin-bottom-4"><div class="govuk-tag govuk-tag--purple">${text}</div></div>`,
-  });
-}
--- src/journeys/view-application/steps/client-details.step.ts
+++ src/journeys/view-application/steps/client-details.step.ts
@@ -1,57 +1,28 @@
 import {
   access,
-  Data,
-  Format,
   step,
-  Transformer,
+  StepDefinition,
 } from "@ministryofjustice/hmpps-forge/core/authoring";
-
-import {
-  APPLICATION_DATA_KEYS,
-  APPLICATIONS_DATA_KEYS,
-  CLIENT_DETAILS_DATA_KEYS,
-  CONTEXT_DATA_KEYS,
-} from "#/journeys/journey.constants.js";
+import { subNavigation } from "#/journeys/view-application/steps/client-details.blocks.js";
 import {
   caseReferenceNumber,
   heading,
   printButton,
   recordedOn,
   statusTag,
-} from "#/journeys/view-application/steps/client-details.blocks.js";
+} from "#/journeys/view-application/common.blocks.js";
 import { viewApplicationEffects } from "#/journeys/view-application/viewApplication.effects.js";
 
-const referenceNumber = Data(CONTEXT_DATA_KEYS.application).path(
-  APPLICATION_DATA_KEYS.applicationRefNumber,
-);
-
-const recordedOnDate = Data(CONTEXT_DATA_KEYS.application)
-  .path(APPLICATIONS_DATA_KEYS.modifiedAt)
-  .pipe(
-    Transformer.String.FormatDate({
-      day: "numeric",
-      locale: "en-GB",
-      month: "short",
-      timeZone: "Europe/London",
-      year: "numeric",
-    }),
-  );
-
-const clientName = Format(
-  "%1 %2",
-  Data(CONTEXT_DATA_KEYS.application).path(CLIENT_DETAILS_DATA_KEYS.firstName),
-  Data(CONTEXT_DATA_KEYS.application).path(CLIENT_DETAILS_DATA_KEYS.lastName),
-);
-
-export const clientDetailsStep = (): ReturnType<typeof step> =>
+export const clientDetailsStep = (): StepDefinition =>
   step({
     backlink: "/cases/recorded",
     blocks: [
       statusTag("Recorded"),
-      heading(clientName),
-      caseReferenceNumber(referenceNumber),
-      recordedOn(recordedOnDate),
+      heading(),
+      caseReferenceNumber(),
+      recordedOn(),
       printButton(),
+      subNavigation(),
     ],
     onAccess: [
       access({
