uk-gov-mirror/ministryofjustice.laa-record-controlled-work

rename answer enum

src/journeys/create-application/steps/ecf/ecf.step.ts
@@ -7,7 +7,7 @@ import {
   type SubmitHook,
 } from "@ministryofjustice/hmpps-forge/core/authoring";
 
-import { AnswerKey } from "#/journeys/AnswerKey.enum.js";
+import { AnswerValue } from "#/journeys/AnswerValues.enum.js";
 import { CreateApplicationEffects } from "#/journeys/create-application/create-application.effects.js";
 import {
   declarationPath,
@@ -58,7 +58,7 @@ const onSubmission = (journeyCode: string): SubmitHook =>
 
 const redirectToECFDropout = redirect({
   goto: StepCode.ECF_DROPOUT,
-  when: Answer(StepCode.ECF).match(Condition.Equals(AnswerKey.YES)),
+  when: Answer(StepCode.ECF).match(Condition.Equals(AnswerValue.YES)),
 });
 
 const redirectToLegalAidBefore = redirect({ goto: StepCode.LEGAL_AID_BEFORE });

ministryofjustice/laa-record-controlled-work

src/journeys/create-application/steps/ecf/ecf.step.ts
@@ -8,7 +8,6 @@ import {
   type SubmitHook,
 } from "@ministryofjustice/hmpps-forge/core/authoring";
 
-import { AnswerKey } from "#/journeys/AnswerKey.enum.js";
 import { CreateApplicationEffects } from "#/journeys/create-application/create-application.effects.js";
 import { ecfQuestion } from "#/journeys/create-application/steps/ecf/ecf.blocks.js";
 import { JourneyPath } from "#/journeys/JourneyPath.enum.js";
@@ -20,6 +19,7 @@ import {
 import { redirectToCheckAnswers } from "#/journeys/shared.hook.js";
 import { StepCode } from "#/journeys/StepCode.enum.js";
 import { t } from "#/lib/i18n.js";
+import { AnswerKey } from "#/journeys/AnswerKey.enum.js";
 
 const DECLARATION_PATH = `${JourneyPath.CREATE_APPLICATION}/provider-declaration`;
 const TITLE = t("journeys.createApplication.ecf.title");