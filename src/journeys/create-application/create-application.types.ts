a462a2f412767f30b55645b22a63fcbef946b40c
Drew Clayton <drew.clayton@justice.gov.uk>
2026-09-15 11:38:12 +0100

    updatred answers type

--- src/journeys/create-application/create-application.types.ts
+++ src/journeys/create-application/create-application.types.ts
@@ -1,10 +1,33 @@
 import type { EffectFunctionContext } from "@ministryofjustice/hmpps-forge/core";
 
 import type { createApplication } from "#/api/clients/rcw/schema/applications/applications.gen.js";
+import type { AnswerKey } from "#/journeys/AnswerKey.enum.js";
 import type { JourneySession } from "#/journeys/context.type.js";
 import type { CONTEXT_DATA_KEYS } from "#/journeys/journey.constants.js";
 
-export interface CreateApplicationAnswers extends Record<string, unknown> {}
+export interface CreateApplicationAnswers extends Record<string, unknown> {
+  [AnswerKey.dateOfBirth]?: string;
+  [AnswerKey.ecf]?: string;
+  [AnswerKey.firstName]?: string;
+  [AnswerKey.hasNINumber]?: string;
+  [AnswerKey.haveAHomeAddress]?: string;
+  [AnswerKey.lastName]?: string;
+  [AnswerKey.legalAidBefore]?: string;
+  [AnswerKey.legalAidLast6Months]?: string;
+  [AnswerKey.niNumber]?: string;
+  [AnswerKey.osAddressLine1]?: string;
+  [AnswerKey.osAddressLine2]?: string;
+  [AnswerKey.osAddressLine3]?: string;
+  [AnswerKey.osAddressLine4]?: string;
+  [AnswerKey.osCountry]?: string;
+  [AnswerKey.reasonForYes]?: string;
+  [AnswerKey.ukAddressLine1]?: string;
+  [AnswerKey.ukAddressLine2]?: string;
+  [AnswerKey.ukCountry]?: string;
+  [AnswerKey.ukCounty]?: string;
+  [AnswerKey.ukPostcode]?: string;
+  [AnswerKey.ukTownOrCity]?: string;
+}
 
 export type CreateApplicationContext = EffectFunctionContext<
   CreateApplicationData,
